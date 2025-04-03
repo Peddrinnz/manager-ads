89b61a4012a386e7fc660ffba505d891936fc510
Pedro <pedroesnarriaga@gmail.com>
2025-04-03 18:54:15 -0400

    feat: create, edit and delete methods in student controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -13,9 +13,66 @@ class StudentController extends Controller
         return view('students.index', compact('students'));
     }
 
+    public function create()
+    {
+        return view('students.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'cpf' => 'required|unique:students|max:14',
+            'name' => 'required|max:100',
+            'date_of_birth' => 'required|date',
+        ]);
+
+        Student::create($validated);
+
+        return redirect()->route('students.index')
+                         ->with('success', 'Estudante criado com sucesso!');
+    }
+
     public function show($id)
     {   
-        $students = Student::findOrFail($id);
-        return view('students.show', compact('students'));
+        $student = Student::findOrFail($id);
+        return view('students.show', compact('student'));
+    }
+
+    public function edit($id)
+    {
+        $student = Student::findOrFail($id);
+        return view('students.edit', compact('student'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $student = Student::findOrFail($id);
+        
+        $validated = $request->validate([
+            'cpf' => 'required|max:14|unique:students,cpf,'.$student->id,
+            'name' => 'required|max:100',
+            'date_of_birth' => 'required|date',
+        ]);
+
+        $student->update($validated);
+
+        return redirect()->route('students.index')
+                         ->with('success', 'Estudante atualizado com sucesso!');
+    }
+
+    public function destroy($id)
+    {
+        $student = Student::findOrFail($id);
+        $student->delete();
+
+        return redirect()->route('students.index')
+                         ->with('success', 'Estudante removido com sucesso!');
+    }
+    public function removeStudent(Request $request, $classroomId, $studentId)
+    {
+    $classroom = Classroom::findOrFail($classroomId);
+    $classroom->students()->detach($studentId);
+
+    return back()->with('success', 'Estudante removido da turma!');
     }
 }
